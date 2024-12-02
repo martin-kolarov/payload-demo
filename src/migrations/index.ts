d698f8cfa35441cb3bfd3e98d7beea13c8324d9e
Martin Kolarov <martin.kolarov@psp-webtech.co.uk>
2024-12-02 18:47:16 +0200

    Update package manager & create migrations

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20241202_164633 from './20241202_164633';
+
+export const migrations = [
+  {
+    up: migration_20241202_164633.up,
+    down: migration_20241202_164633.down,
+    name: '20241202_164633'
+  },
+];
